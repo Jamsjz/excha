a8dd6eb10c59a4af27eca5ff68b4f45b9c9dc412
j <gj984233@gmail.com>
2024-12-16 11:45:24 +0545

    new

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.tsx",
-            refresh: true,
-        }),
-        react(),
-    ],
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        manifest: true,
+  plugins: [
+    laravel({
+      input: "resources/js/app.tsx",
+      refresh: true,
+    }),
+    react(),
+  ],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    manifest: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./resources/js"),
+      "~": path.resolve(__dirname, "./public"),
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            '~': path.resolve(__dirname, './public')
-        }
-    },
-    publicDir: 'public',
+  },
+  publicDir: "public",
 });
